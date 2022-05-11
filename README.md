8c568f633e4361f8fb04167ff67fa36cc1fe65c1
Kolawole Kushimo <kolawole.kushimo@andela.com>
2022-05-11 01:26:04 +0100

    [NEW] Ingesting data from a website

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Python_Ingestion
 Data Engineering
+
+- Running pip install on mac
+    - `https://www.mytecbits.com/internet/python/installing-python-packages`
+    - Search and start Terminal: Create New Integrated Terminal. The terminal panel will open at the bottom.
+    - In the terminal run these comments.
+        `python3 -m venv env`
+        `source env/bin/activate`
+
+    - A new virtual environment called env is now created for the workspace. To see this, go to the explorer panel and expand the newly created folder under the workspace.
+
+    - Open the Command Palette again and search and select Python: Select Interpreter. Now you will see the newly created virtual environmnet (something similar to Python 3.7.3 64-bit (‘env’, venv)). Select the virtual environment as your interpreter.
+
+    - Virtual environment is ready. Now we will see how to install a Python package.
---
+++ exchange_data.py
@@ -0,0 +1,32 @@
+# Run in terminal after setting up virtual envr - 
+# python3 -m pip install schedule
+# python3 -m pip install requests
+
+import schedule # pip3 install schedule
+import requests # pip3 install request
+import time
+import json
+
+def pull_currency_data(site_url, apikey, file_name):
+    """
+    :param site_url: URL to which requests get should hit
+    :param apikey: Key from your account
+    :param file_name: location and file name where output should be saved/appended
+    """
+    global data
+    data = requests.get(url=site_url+apikey)
+    file = open(file_name, "a")
+    currency = json.loads(data.content)
+    output = data.headers['Date'] + '|' + '1' + '|' + str(currency['USD']) + '|' + str(currency['JPY']) + '|' + \
+             str(currency['EUR']) + '|' + str(currency['USD']) + '\n'
+    file.write(output)
+    file.close()
+
+key = '2447285030a725a86d4ef48ea1336edb9c67189caa0750fb8ab4697799861dc8'
+url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR,NGN&api_key='
+file_name = './Users/kolawole/Documents/currency_extract.csv'
+schedule.every(30).seconds.do(pull_currency_data, url, key, file_name)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
